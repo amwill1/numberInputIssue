1a632242d7e4d85acfc3f1d9a201734ab2f62779
Amanda Williamson <amandamwill11@gmail.com>
2016-01-27 14:49:34 -0700

    update all files

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var React = require('react');
 
+console.log(React);
+
 var App = React.createClass({
   render: function () {
     return (
       <div>
-        <input id="numInput" type="number" min="1" max="100" />
+        <input id="numInput" type="number" min="1" max="100" value="20" />
         <input id="textInput" type="text" value="hello there" />
       </div>
-    )
+    );
   }
 });
 
-React.render({
+React.render(
   <App />,
-  document.getElementById('content');
-})
+  document.getElementById('content')
+);
--- test.js
+++ test.js
@@ -1,2 +1,16 @@
-var webdriver = require('../node_modules/selenium-webdriver');
+var webdriver = require('./node_modules/selenium-webdriver');
 var seleniumAddress = "http://localhost:4444/wd/hub";
+
+var driver = new webdriver.Builder().
+    usingServer(seleniumAddress).
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
+
+driver.get('http://amwill1.github.io/numberInputIssue/index.html');
+driver.sleep(5000);
+var numInput = driver.findElement(webdriver.By.id('numInput'));
+var textInput = driver.findElement(webdriver.By.id('textInput'));
+numInput.clear();
+driver.sleep(5000);
+textInput.clear();
+driver.sleep(5000);
